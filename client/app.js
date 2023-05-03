014237a67ab96b97d13bd74fe5a32d19c8f7fb64
Konstanty Wrona <koswro@gmail.com>
2023-05-03 06:44:21 +0200

    Add userName

--- client/app.js
+++ client/app.js
@@ -1,6 +1,10 @@
+// HTML references
 const loginForm = document.querySelector('#welcome-form');
 const messagesSection = document.querySelector('#messages-section');
 const messagesList = document.querySelector('#messages-list');
 const addMessagesForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
+
+// Global
+let userName = '';
