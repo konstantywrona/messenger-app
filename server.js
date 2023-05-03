28e8bba748ab196ab6d186b6f85530746d71bea8
Konstanty Wrona <koswro@gmail.com>
2023-05-03 07:46:23 +0200

    Add socet listeners

--- server.js
+++ server.js
@@ -15,3 +15,11 @@ app.use(express.static(path.join(__dirname, '/client/')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/index.html'));
 });
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', () => {
+    console.log("Oh, I've got something from " + socket.id);
+  });
+  console.log("I've added a listener on message event \n");
+});
