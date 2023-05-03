6bd05f57932b308e9d190e03347770657008876d
Konstanty Wrona <koswro@gmail.com>
2023-05-03 06:52:06 +0200

    Add login & sendMessage

--- client/app.js
+++ client/app.js
@@ -8,3 +8,28 @@ const messageContentInput = document.querySelector('#message-content');
 
 // Global
 let userName = '';
+
+const login = (e) => {
+  e.preventDefault();
+  if (userNameInput.value.length > 0) {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+    addMessagesForm.classList.add('show');
+  } else {
+    alert('Please enter a username');
+  }
+};
+
+const sendMessage = (e) => {
+  e.preventDefault();
+  if (messageContentInput.value.length > 0) {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  } else {
+    alert('Please enter a message');
+  }
+};
+
+loginForm.addEventListener('submit', (e) => login(e));
+addMessagesForm.addEventListener('submit', (e) => sendMessage(e));
