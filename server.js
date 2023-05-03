47d24ee119166212905a8c939b69feaa7fb1c785
Konstanty Wrona <koswro@gmail.com>
2023-05-03 07:19:48 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const message = [];
+
+app.use(express.static(path.join(__dirname, '/client/')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+app.listen(8000, () => {
+  console.log('Server is runing on port 8000');
+});
